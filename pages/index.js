084614a51be1ecf385209e3bf27043c9e99bc060
Omar Maroki <omar987@hotmail.co.uk>
2022-06-06 23:45:40 +0100

    fix typo

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
 import Hero from '../components/Hero/Hero';
 import Navbar from '../components/Navbar/Navbar';
-import Showcase from '../components/Showcase/ShowCase';
+import Showcase from '../components/Showcase/Showcase';
 
 export default function Home() {
 	return (
