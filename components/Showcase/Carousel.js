7fc6c003e72f781e5fba10a44294469bedbf3f0e
Omar Maroki <omar987@hotmail.co.uk>
2022-06-07 18:06:24 +0100

    switched swiperjs for alice carousel

--- components/Showcase/Carousel.js
+++ components/Showcase/Carousel.js
@@ -1,55 +1,102 @@
 import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-cards';
-
-// import required modules
-import { EffectCards } from 'swiper';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import Image from 'next/image';
-
+import AliceCarousel from 'react-alice-carousel';
+const style = {
+	width: '70vw',
+	height: '50vh',
+	position: 'relative',
+	margin: '0 auto',
+};
+const responsive = {
+	0: { items: 1 },
+	768: { items: 1.2 },
+};
 export default function Carousel() {
-	return (
-		<Swiper
-			effect={'cards'}
-			grabCursor={true}
-			modules={[EffectCards]}
-			className="mySwiper"
-		>
-			<SwiperSlide>
-				<Image
-					src={'/9.svg'}
-					alt="product-slide-1"
-					layout="fill"
-					objectFit="cover"
-				/>
-			</SwiperSlide>
-			<SwiperSlide>
-				<Image
-					src={'/10.svg'}
-					alt="product-slide-2"
-					layout="fill"
-					objectFit="cover"
-				/>
-			</SwiperSlide>
-			<SwiperSlide>
-				<Image
-					src={'/11.svg'}
-					alt="product-slide-3"
-					layout="fill"
-					objectFit="cover"
-				/>
-			</SwiperSlide>
-			<SwiperSlide>
-				<Image
-					src={'/12.svg'}
-					alt="product-slide-4"
-					layout="fill"
-					objectFit="cover"
-				/>
-			</SwiperSlide>
-		</Swiper>
-	);
+	const handleDragStart = (e) => e.preventDefault();
+
+	const items = [
+		<div key="vdces" style={style}>
+			<Image
+				src={'/9.svg'}
+				alt="product-slide-1"
+				layout="fill"
+				objectFit="cover"
+				onDragStart={handleDragStart}
+				role="presentation"
+			/>
+		</div>,
+		<div key="rfdrf" style={style}>
+			<Image
+				src={'/10.svg'}
+				alt="product-slide-1"
+				layout="fill"
+				objectFit="cover"
+				onDragStart={handleDragStart}
+				role="presentation"
+			/>
+		</div>,
+		<div key="bgyh" style={style}>
+			<Image
+				src={'/11.svg'}
+				alt="product-slide-1"
+				layout="fill"
+				objectFit="cover"
+				onDragStart={handleDragStart}
+				role="presentation"
+			/>
+		</div>,
+		<div key="refew" style={style}>
+			<Image
+				src={'/12.svg'}
+				alt="product-slide-1"
+				layout="fill"
+				objectFit="cover"
+				onDragStart={handleDragStart}
+				role="presentation"
+			/>
+		</div>,
+	];
+
+	return <AliceCarousel mouseTracking items={items} responsive={responsive} />;
 }
+
+// <Swiper
+// 	effect={'cards'}
+// 	grabCursor={true}
+// 	modules={[EffectCards]}
+// 	className="mySwiper"
+// >
+// 	<SwiperSlide>
+// 		<Image
+// 			src={'/9.svg'}
+// 			alt="product-slide-1"
+// 			layout="fill"
+// 			objectFit="cover"
+// 		/>
+// 	</SwiperSlide>
+// 	<SwiperSlide>
+// 		<Image
+// 			src={'/10.svg'}
+// 			alt="product-slide-2"
+// 			layout="fill"
+// 			objectFit="cover"
+// 		/>
+// 	</SwiperSlide>
+// 	<SwiperSlide>
+// 		<Image
+// 			src={'/11.svg'}
+// 			alt="product-slide-3"
+// 			layout="fill"
+// 			objectFit="cover"
+// 		/>
+// 	</SwiperSlide>
+// 	<SwiperSlide>
+// 		<Image
+// 			src={'/12.svg'}
+// 			alt="product-slide-4"
+// 			layout="fill"
+// 			objectFit="cover"
+// 		/>
+// 	</SwiperSlide>
+// </Swiper>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.scss';
-import 'swiper/css';
-import 'swiper/css/effect-cards';
+
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
